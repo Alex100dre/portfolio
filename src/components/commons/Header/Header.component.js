3dd188c7e60a5a69ca00690bc4ac40b1a60ffefe
Alexandre Hachim <alex100dre.pro@gmail.com>
2019-02-27 20:17:35 +0100

    feat(Header): Allow header to have content

--- src/components/commons/Header/Header.component.js
+++ src/components/commons/Header/Header.component.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {
   HeaderStyled,
+  Background,
+  Content,
 } from './Header.style'
+import Button from '../Button/Button.component'
 
 export default class Header extends Component {
   render() {
-    const { bgImage, bgColor, blur } = this.props
+    const {
+      bgImage, bgColor, blur, children,
+    } = this.props
     return (
-      <HeaderStyled bgImage={bgImage} bgColor={bgColor} blur={blur} />
+      <HeaderStyled bgImage={bgImage} bgColor={bgColor} blur={blur}>
+        {(bgImage || bgColor) && (
+          <Background bgImage={bgImage} bgColor={bgColor} blur={blur} />
+        )}
+
+        {children && (
+          <Content>
+            {children}
+          </Content>
+        )}
+      </HeaderStyled>
     )
   }
 }
@@ -17,10 +32,12 @@ Header.propTypes = {
   bgImage: PropTypes.string,
   bgColor: PropTypes.string,
   blur: PropTypes.bool,
+  children: PropTypes.node,
 }
 
 Header.defaultProps = {
   bgImage: null,
   bgColor: '#ff0050',
   blur: false,
+  children: null,
 }
--- src/components/commons/Header/Header.style.js
+++ src/components/commons/Header/Header.style.js
@@ -3,10 +3,23 @@ import styled from 'styled-components'
 export const HeaderStyled = styled.header`
   position: relative;
   min-height: 300px;
+  padding: 40px 0;
+  box-sizing: border-box;
+`
+
+export const Background = styled.div`
   background: ${({ bgImage, bgColor }) => (bgImage ? `url(${bgImage})` : bgColor)} no-repeat center;
   background-size: cover;
-  
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   ${({ blur }) => blur && `
     filter: blur(8px);
   `}
+`
+
+export const Content = styled.div`
+  
 `
