81b57b1525163dd2c00821f945c6da9114cee6aa
Alexandre Hachim <alex100dre.pro@gmail.com>
2019-03-02 00:48:53 +0100

    feat(Button): Allow to wrap Button in a styled component

--- src/components/commons/Button/Button.component.js
+++ src/components/commons/Button/Button.component.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types'
 import { Btn, BtnLink } from './Button.style'
 
 const Button = ({
-  types, onClick, href, children,
+  types, onClick, href, children, ...others
 }) => {
   const classes = types.join(' ')
 
   if (href) {
     return (
-      <BtnLink href={href} className={classes}>
+      <BtnLink href={href} className={classes} {...others}>
         {children}
       </BtnLink>
     )
   }
 
   return (
-    <Btn onClick={onClick} className={classes}>
+    <Btn onClick={onClick} className={classes} {...others}>
       {children}
     </Btn>
   )
