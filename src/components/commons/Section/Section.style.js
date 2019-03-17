278446de16fa6e2d3d1483799fcee7fad94d8c86
Alexandre Hachim <alex100dre.pro@gmail.com>
2019-03-17 18:24:16 +0100

    feat(Section): FIX background cover

--- src/components/commons/Section/Section.style.js
+++ src/components/commons/Section/Section.style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Sctn = styled.section`
   padding: ${props => (props.noPadding ? '0' : '20px 0')};
   background: ${({ bgImage }) => (bgImage ? `url(${bgImage})` : 'none')} no-repeat center;
+  background-size: cover;
 `
 
 export const SectionTitle = styled.h2`
