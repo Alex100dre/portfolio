ef6c51b2b22304b6eaf8e4fbc04066371d8783c8
Alexandre <alex100dre.pro@gmail.com>
2021-10-28 01:32:21 +0200

    chore: change portfolio api url

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -1,6 +1,6 @@
 export default {
   PORTFOLIO_SERVER: {
-    DOMAIN: 'https://portfolio-server.le-dev.com',
+    DOMAIN: 'https://portfolio-back.le-dev.com',
     ENDPOINTS: {
       PROJECTS: '/projects'
     },
